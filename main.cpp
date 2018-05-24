c01e25496b0e0011fa05c84f1600723d3907db92
teodoranemtanu <narcisa-teodora.nemtanu@my.fmi.unibuc.com>
2018-05-25 02:01:40 +0300

    reads the data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+using namespace std;
+ifstream fin("..\\date.in");
+ofstream fout("..\\date.out");
+
+string word;
+int nrTerminals, nrNonterminals;
+vector<char> nonterminals;
+vector<char> terminals;
+map<char, vector<string> > productions;
+int nrProductions;
+///A -> AA / aBa / lambda(codified with #)
+char start;
+
+void readGrammar() {
+    fin >> nrTerminals;
+    terminals.resize(nrTerminals);
+    for (int i = 0; i < nrTerminals; i++)
+        fin >> terminals[i];
+    fin >> nrNonterminals;
+    nonterminals.resize(nrNonterminals);
+    for (int i = 0; i < nrTerminals; i++)
+        fin >> nonterminals[i];
+    fin >> nrProductions;
+    for(int i = 0; i < nrProductions; i++){
+        char leftMember;
+        string rightMember;
+        fin >> leftMember >> rightMember;
+        productions[leftMember].push_back(rightMember);
+    }
+    fin >> start;
+}
+
+bool isAccepted(string word){
+    queue <>
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin >> word;
+    readGrammar();
+
     return 0;
 }
